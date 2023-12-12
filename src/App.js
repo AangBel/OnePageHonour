6bf573b979c4eae7f6db2f1d81e2709c5e8f8249
Angelica Beltran <angelica.beltran11@gmail.com>
2023-12-12 03:10:49 -0600

    adjusting css for navigation bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 
 import Nav from "../src/components/Nav/Nav";
 
+// import "../public/Fonts/FontAwesome5BrandsRegular.ttf";
+
 function App() {
   return (
     <div className="App">
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -10,32 +10,39 @@ import "./Nav.css";
 function Nav() {
   return (
     <div className="nav">
-      {/* <Link to="/About">
-        <h2 className="nav-title">Honour</h2>
-      </Link> */}
-      <img src={HONOUR_PRIMARY_logo} alt="Honour Coffee Logo" />
-      <ul>
-        <li>THE BLENDS</li>
-        <li>OUR STORY</li>
-      </ul>
-      <div>
-        <>
-          {/* It doesnt look like we will need Link for example because its just one page... */}
-          {/* <Link className="navLink" to="/TheBlends">
-           The Blends
-          </Link>
-
-          <Link className="navLink" to="/OurStory">
-            Our Story
-          </Link> */}
-
-          {/* <Link className="navLink" to="/Contact">
-            Contact
-          </Link> */}
-        </>
+      <div className="logo">
+        <img src={HONOUR_PRIMARY_logo} alt="Honour Coffee Logo" />
+      </div>
+      <div className="navList">
+        <ul>
+          <li>THE BLENDS</li>
+          <li>OUR STORY</li>
+        </ul>
       </div>
     </div>
   );
 }
 
 export default Nav;
+{
+  /* <Link to="/About">
+        <h2 className="nav-title">Honour</h2>
+      </Link> */
+}
+/* It doesnt look like we will need Link for example because its just one page... */
+
+{
+  /* <Link className="navLink" to="/TheBlends">
+           The Blends
+          </Link>
+
+          <Link className="navLink" to="/OurStory">
+            Our Story
+          </Link> */
+}
+
+{
+  /* <Link className="navLink" to="/Contact">
+            Contact
+          </Link> */
+}
