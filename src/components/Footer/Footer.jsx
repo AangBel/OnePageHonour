9eba969900160a6805c4c45b84d1fd7203134b4a
Angelica Beltran <angelica.beltran11@gmail.com>
2023-12-16 01:38:04 -0600

    added the footer legalese pieces

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,10 +5,27 @@ import "./Footer.css";
 function Footer() {
   console.log("stuff");
   return (
-    <div className="footerHonourText">
+    <>
+      <div className="footerHonourText">
         <h3>START YOUR DAY WITH HONOUR</h3>
-    </div>
-  )
+      </div>
+      <div className="footerEmailInput">
+        <label for="email">Email address</label>
+
+        <input type="email" id="email" pattern=".+@example\.com" size="30" placeholder="Email Address" />
+      </div>
+      <div className="legaleseFooter">
+        <p>©2023 HONOUR COFFEE</p>
+        <br></br>
+        <p>PRIVACY POLICY</p>
+        <br></br>
+
+        <p>TERMS & CONDITIONS</p>
+        <br></br>
+
+      </div>
+    </>
+  );
 }
 
 export default Footer;
