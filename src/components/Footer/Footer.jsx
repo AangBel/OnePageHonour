f1324ee83576250098a0305afa1815f7295ee600
Angelica Beltran <angelica.beltran11@gmail.com>
2023-12-17 04:49:25 -0600

    at some point i need to figure out the image paths not being lazy vs and just being in folder? or priorities elsewhere? added contact links at footer and the secondary honour logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 import "./Footer.css";
 
+import SecondaryLogo from "./SecondaryLogo.svg";
+
 function Footer() {
   console.log("stuff");
   return (
     <>
       <div className="footerHonourText">
+        <img src={SecondaryLogo}></img>
         <h3>START YOUR DAY WITH HONOUR</h3>
       </div>
       <div className="footerEmailInput">
@@ -14,6 +17,18 @@ function Footer() {
 
         <input type="email" id="email" pattern=".+@example\.com" size="30" placeholder="Email Address" />
       </div>
+
+      <div className="contactLinks">
+
+        <h3>Help Desk</h3>
+        <h3>Press</h3>
+        <h3>Carry Us</h3>
+
+
+      </div>
+
+
+
       <div className="legaleseFooter">
         <p>©2023 HONOUR COFFEE</p>
         <br></br>
