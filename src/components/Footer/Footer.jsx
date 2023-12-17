71d35ace02fbf3faed651ebfca702427052f89a1
Angelica Beltran <angelica.beltran11@gmail.com>
2023-12-17 17:09:32 -0600

    editing footer css for svg and legalese alignment

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-
 // import {icon} from '@fortawesome/fontawesome-svg-core/import.macro';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebookF, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
-// import { solid, icon } from '@fortawesome/fontawesome-svg-core/import.macro' 
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+// import { solid, icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 import "./SocialIcons/facebook-f.svg";
 import "./SocialIcons/x-twitter.svg";
 import "./SocialIcons/youtube.svg";
-
 
 import "./Footer.css";
 
@@ -28,36 +28,31 @@ function Footer() {
       <div className="footerEmailInput">
         <label for="email">Email address</label>
 
-        <input type="email" id="email" pattern=".+@example\.com" size="30" placeholder="Email Address" />
+        <input
+          type="email"
+          id="email"
+          pattern=".+@example\.com"
+          size="30"
+          placeholder="Email Address"
+        />
       </div>
 
       <div className="contactLinks">
-
         <h3>Help Desk</h3>
         <h3>Press</h3>
         <h3>Carry Us</h3>
-
-
       </div>
 
-    
+      <div className="legaleseFooter">
+        <p>©2023 HONOUR COFFEE</p>
+        <p>PRIVACY POLICY</p>
+        <p>TERMS & CONDITIONS</p>
+      </div>
+
       <div className="socialLinks">
         <FontAwesomeIcon icon={faFacebookF} />
         <FontAwesomeIcon icon={faTwitter} />
         <FontAwesomeIcon icon={faYoutube} />
-      </div>
-    
-
-
-      <div className="legaleseFooter">
-        <p>©2023 HONOUR COFFEE</p>
-        <br></br>
-        <p>PRIVACY POLICY</p>
-        <br></br>
-
-        <p>TERMS & CONDITIONS</p>
-        <br></br>
-
       </div>
     </>
   );
