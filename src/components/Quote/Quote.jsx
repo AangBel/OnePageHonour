765915e9d48d13d33828fe8cc70e284a506d2533
Angelica Beltran <angelica.beltran11@gmail.com>
2023-12-20 21:04:46 -0600

    rearranging hero text padding and quote text

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -6,11 +6,11 @@ function Quote() {
     <div className="quoteSection">
       <div className="quoteBox">
         <p className="quotePText">
-        Cupcake ipsum dolor sit amet icing cookie cupcake. Sweet roll bear
+          Cupcake ipsum dolor sit amet icing cookie cupcake. Sweet roll bear
           claw danish soufflé lemon drops toffee sweet pastry chocolate. Sesame
           snaps cotton candy topping tiramisu chupa chups candy canes apple pie
           muffin. Sweet roll tootsie roll wafer donut bonbon cake chocolate
-          macaroon ice cream. Halvah cake liquorice marshmallow pastry. 
+          macaroon ice cream. Halvah cake liquorice marshmallow pastry.
         </p>
         <p className="quoteEllipse"></p>
         <div className="ownerNameTitleBox">
