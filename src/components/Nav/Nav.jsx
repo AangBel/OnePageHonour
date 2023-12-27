740b2007d784c3f05fd8345c82a583f5810573f0
Angelica Beltran <angelica.beltran11@gmail.com>
2023-12-27 00:27:23 -0600

    adding the movile nav menu

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Nav.css";
 
 import HONOUR_PRIMARY_logo from "./HONOUR_PRIMARY_logo.svg";
 import "./Nav.css";
 
 function Nav() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="nav">
       <div className="logo">
@@ -14,18 +20,41 @@ function Nav() {
           alt="Honour Coffee Logo"
         />
       </div>
+
       <div className="navAnchors">
         <a href="#TheBlends" className="navAnchor1">
           THE BLENDS
         </a>
-
         <a href="#OurStory" className="navAnchor2">
           OUR STORY
         </a>
       </div>
+
       <div className="shopContainer">
         <p className="shopSymbol">SHOP</p>
       </div>
+
+      
+          <div className="menu" onClick={toggleMenu}>
+            MENU
+          </div>
+
+      {showMenu && (
+        <>
+
+          <div className="mobileMenu">
+            <a href="#TheBlends" className="navAnchor1">
+              THE BLENDS
+            </a>
+            <a href="#OurStory" className="navAnchor2">
+              OUR STORY
+            </a>
+            <div className="shopContainer">
+              <p className="shopSymbol">SHOP</p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
