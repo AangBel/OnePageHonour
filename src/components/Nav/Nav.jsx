9c32cbc5ddd3a5556affbe8feec5d2a579c338eb
Angelica Beltran <angelica.beltran11@gmail.com>
2024-01-01 14:03:31 -0600

    editing nav now honour logo appearing but menu for mobile is coming up as regular nav and not as the mobile nav i designated

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,6 +3,7 @@ import "./Nav.css";
 
 import HONOUR_PRIMARY_logo from "./HONOUR_PRIMARY_logo.svg";
 import "./Nav.css";
+import "./NavMobile.css";
 
 function Nav() {
   const [showMenu, setShowMenu] = useState(false);
